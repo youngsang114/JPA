73868cfbbfb84950e3868249dcb131143ac64ff8
yys <dgh06008@naver.com>
2024-02-12 17:34:26 +0900

    24.02.12

---
+++ jpashop/src/test/java/jpabook/jpaShop/JpaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.jpaShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -14,34 +14,55 @@ public class JpaMain {
 
         try {
 
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
 
-            Member member = new Member();
-            member.setUsername("member");
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
-            em.persist(member);
+            Team teamB = new Team();
+            teamB.setName("팀A");
+            em.persist(teamB);
 
-            em.flush();
-            em.clear();
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
 
-//            List<Member> resultList = em.createQuery("select m from Member m order by m.age desc", Member.class)
-//                    .setFirstResult(1)
-//                    .setMaxResults(10)
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
+
+//            em.flush();
+//            em.clear();
+
+            int resultCount = em.createQuery("update Member m set m.age =20")
+                    .executeUpdate();
+            System.out.println("resultCount = " + resultCount);
+
+            em.clear(); // bulk 연산을 위해서 필수
+
+
+            Member findMember = em.find(Member.class, member1.getId());
+            System.out.println(findMember.getAge());
+
+
+//            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
+//                    .setParameter("username", member1.getUsername())
 //                    .getResultList();
-//            System.out.println("resultList.size() = " + resultList.size());
-//            for (Member member1 : resultList) {
-//                System.out.println("member1 = " + member1);
+//            for (Member member : resultList) {
+//                System.out.println("member = " + member);
 //            }
-            String query = "select m.username, 'hello',true from Member m " +
-                    "where m.type = : userType";
-            List<Object[]> resultList = em.createQuery(query)
-                    .setParameter("userType", MemberType.ADMIN)
-                    .getResultList();
-            for (Object[] objects : resultList) {
-                System.out.println("objects[0] = " + objects[0]);
-                System.out.println("objects[0] = " + objects[1]);
-                System.out.println("objects[0] = " + objects[2]);
-            }
+
+//            String query = "select m from Member m where m= :member";
+//
+//            Member findMember = em.createQuery(query, Member.class)
+//                    .setParameter("member", member1).getSingleResult();
+//            System.out.println("findMember = " + findMember);
 
 
             tx.commit();
