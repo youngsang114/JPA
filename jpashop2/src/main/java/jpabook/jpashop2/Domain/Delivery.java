98a3bc95e2926dff8fb1607d7291ff7a80bf2685
yys <dgh06008@naver.com>
2024-02-12 23:14:07 +0900

    24.02.12

--- jpashop2/src/main/java/jpabook/jpashop2/Domain/Delivery.java
+++ jpashop2/src/main/java/jpabook/jpashop2/Domain/Delivery.java
@@ -11,7 +11,7 @@ public class Delivery {
     @Column(name = "deliver_id")
     private Long id;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery",fetch = FetchType.LAZY)
     private Order order;
 
     @Embedded
